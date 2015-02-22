83ba785abd18493bd10bb57c2d227b840e7f250b
Mat Moore <matmoore@users.noreply.github.com>
2015-02-22 16:40:28 +0000

    Defined method for elimnating candidates

--- src/main/scala/stv/Candidate.scala
+++ src/main/scala/stv/Candidate.scala
@@ -22,7 +22,19 @@ case class Election(candidates: Set[Candidate], votes: List[List[Candidate]] = L
  *                 (the vote may be transferred to less preferred candidates
  *                 throughout the counting process).
  */
-case class AllocatedVote(vote: List[Candidate], selected: Candidate)
+case class AllocatedVote(vote: List[Candidate], selected: Candidate) {
+  /**
+   * Transfer the vote to the next best choice
+   * @param eliminated All candidates that have been eliminated from the election
+   * @return New allocation for this vote
+   */
+  def transfer(eliminated: Set[Candidate]): Option[AllocatedVote] = {
+    vote.filter(other => !eliminated.contains(other)) match {
+      case head :: tail => Some(AllocatedVote(vote, head))
+      case nil => None
+    }
+  }
+}
 
 /**
  * The method of dealing with surplus votes for a winning candidate.
@@ -91,6 +103,8 @@ trait Count {
    * @return The minimum number of votes a candidate must have to be elected.
    */
   def quota: Int
+  def meetsQuota(numVotes: Int): Boolean = numVotes > quota
+  def meetsQuota(votes: List[AllocatedVote]): Boolean = meetsQuota(votes.length)
 
   // TODO: these may be off by one due to integer division
 
@@ -110,15 +124,55 @@ trait Count {
   def surplusAllocator: SurplusVoteAllocator
 
   /**
+   * Eliminate a single candidate.
+   * @param elected All previously elected candidates
+   * @param hopefuls All candidates that have not been elected or eliminated yet.
+   * @param eliminated All previously eliminated candidates
+   * @return A new list of candidate results.
+   */
+  def eliminate(elected: List[ElectedCandidate], hopefuls: List[HopefulCandidate], eliminated: Set[EliminatedCandidate]): List[CandidateResult] = {
+    require(hopefuls.length > 0)
+
+    val eliminatedSet = eliminated.map(_.candidate)
+    val loser = hopefuls.minBy(candidate => candidate.votes.length)
+    val loserVotes = loser.votes.map(_.transfer(eliminatedSet)).collect {
+      case Some(allocatedVote) => allocatedVote
+    }
+    val loserVotesByCandidate = loserVotes.groupBy(_.selected)
+
+    val stillElected = for (
+      ElectedCandidate(candidate, votes) <- elected
+    ) yield ElectedCandidate(candidate, votes ++ loserVotesByCandidate(candidate))
+
+    val formerHopefuls = for (
+      HopefulCandidate(candidate, votes) <- hopefuls
+    ) yield {
+      val newVotes = votes ++ loserVotesByCandidate(candidate)
+      if (meetsQuota(newVotes)) {
+        ElectedCandidate(candidate, newVotes)
+      } else {
+        HopefulCandidate(candidate, newVotes)
+      }
+    }
+
+    /*
+    The way intermediate CandidateResults are stored is currently a bit haphazard.
+    For now just lump everything into a list
+     */
+    EliminatedCandidate(loser.candidate) :: (stillElected.toList ++ formerHopefuls.toList)
+  }
+
+  /**
    * Count the election.
    * @return A list containing the results per candidate at the end of each
    *         round.
    */
   def roundCounts: List[List[CandidateResult]] = {
-    def eliminate(candidates: List[CandidateResult]): List[CandidateResult]
-
     def isComplete(currentRound: List[CandidateResult]): Boolean = true
 
+    /*
+    Start off with every ballot allocated to the first choice candidate
+     */
     val firstChoiceVotes: Map[Candidate, List[AllocatedVote]] = {
       val allocated = election.votes.map(vote => AllocatedVote(vote, vote.head))
       allocated.groupBy(_ match {
@@ -126,19 +180,44 @@ trait Count {
       })
     }
 
+    /*
+    Elect any candidates that meet the criteria
+     */
     val initialResults: List[CandidateResult] = (for {
       (candidate: Candidate, votes: List[AllocatedVote]) <- firstChoiceVotes
     } yield {
       if (votes.length > quota) ElectedCandidate(candidate, votes) else HopefulCandidate(candidate, votes)
     }).toList
 
+    /**
+     *
+      In each round of counting, we either:
+      1) Transfer surplus votes from newly elected candidates
+      2) Eliminate candidate and transfer their votes
+      We only eliminate when no new candidates can be elected through step 1.
+     * @param currentRound The results as they stand so far.
+     * @return The results after running a new round of counting.
+     */
     def nextRound(currentRound: List[CandidateResult]): List[CandidateResult] = {
       val (reallocatedSurplusResults, newlyElected) = surplusAllocator.reallocate(quota, initialResults)
 
       if(newlyElected) {
         reallocatedSurplusResults
       } else {
-        eliminate(reallocatedSurplusResults)
+        /*
+        FIXME: this is not very nice
+         */
+        val elected = (reallocatedSurplusResults.collect {
+          case ElectedCandidate(candidate, votes) => ElectedCandidate(candidate, votes)
+        }).toList
+        val hopeful = (reallocatedSurplusResults collect {
+          case HopefulCandidate(candidate, votes) => HopefulCandidate(candidate, votes)
+        }).toList
+        val eliminated = (reallocatedSurplusResults collect {
+          case EliminatedCandidate(candidate) => EliminatedCandidate(candidate)
+        }).toSet
+
+        eliminate(elected, hopeful, eliminated)
       }
     }
 
